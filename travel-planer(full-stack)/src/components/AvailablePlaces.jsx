1d5770ebae1fab094f39ae5f0096290d631e28d0
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-07-06 22:02:10 +0100

    Enabled fetching of places data from backend

--- travel-planer(full-stack)/src/components/AvailablePlaces.jsx
+++ travel-planer(full-stack)/src/components/AvailablePlaces.jsx
@@ -1,12 +1,42 @@
 import Places from './Places.jsx';
+import { useEffect, useState } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+
+  const [AVAILABLE_PLACES, setAvailablePlaces] = useState([]);
+
+  useEffect(()=>{
+
+    (async function fetchAvailablePlaces(){
+      const response = await fetch("http://localhost:3000/places");
+      const responseData = await response.json();
+      setAvailablePlaces(responseData.places);
+    })();//execute immediately
+
+  },[])
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={AVAILABLE_PLACES}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
   );
 }
+
+//decided to try out my own code for fetching information from backend via AJAX
+// function placesFetcher(endpointURL, requestData = {type: "GET", body: {}}){
+//   return new Promise((resolve, reject)=>{
+//     const placesRequest = new XMLHttpRequest();
+//     placesRequest.onreadystatechange = function(){
+//       if(this.readyState == 4){
+//         if(this.status == 200){
+//           resolve(JSON.parse(this.responseText));
+//         }
+//       }
+//     };
+//     placesRequest.open(requestData.type, endpointURL, true);
+//     placesRequest.send(JSON.stringify(requestData.body));
+//   });
+// }
