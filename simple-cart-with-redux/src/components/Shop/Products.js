9743117c52273367725689368dcc89a570b7973e
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-08-24 19:07:04 +0100

    Project setup for Async Reducers for Redux
    - Setting a simple shop app

---
+++ simple-cart-with-redux/src/components/Shop/Products.js
@@ -0,0 +1,25 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+import availableProducts from "../../data/availableProducts.json";
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {availableProducts.map(item => 
+          <ProductItem
+            key = {item.id}
+            id = {item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        )}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
